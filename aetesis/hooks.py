ced7c73c85f42804f887c0f3c40d00b29f809da2
aeteseb <sebastian@aetesis.ch>
2022-12-01 15:30:06 +0100

    chore: remove app_include_js from hooks

--- aetesis/hooks.py
+++ aetesis/hooks.py
@@ -25,7 +25,7 @@ my_account_context = "aetesis.e_commerce.shopping_cart.utils.update_my_account_c
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/aetesis/css/aetesis.css"
-app_include_js =  "/assets/js/aetesis.min.js"
+#app_include_js =  "/assets/js/aetesis.min.js"
 
 # include js, css files in header of web template
 web_include_css = "/assets/aetesis/css/aetesis.css"
