9b5cbb1209e2bd74ebb820dd8c5ba1d1620cdf86
aeteseb <sebastian@aetesis.ch>
2022-12-07 22:25:26 +0100

    Revert "show emoji"
    This reverts commit 043ef34ca3429a7b8109aac475cd0efa33751676.

--- aetesis/public/js/regions.js
+++ aetesis/public/js/regions.js
@@ -92,7 +92,7 @@ function get_Card({params, thing, type, country=undefined, language=undefined}) 
 		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check"><polyline points="20 6 9 17 4 12"></polyline></svg>
 	</div>
 	<div class="card-body">
-		<h5 class="card-title align-center emoji">${ thing.flag } ${ thing.country || thing.language }</h5>
+		<h5 class="card-title align-center">${ thing.flag } ${ thing.country || thing.language }</h5>
 	</div>
 </div>`
 }
